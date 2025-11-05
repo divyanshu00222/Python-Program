fb166d62b5ea6ebdce8b45809a711569299d475c
divyanshu00222 <bhanarkardivyanshu2@gmail.com>
2025-11-05 21:19:51 +0530

    hello.py and calculator.py

---
+++ Day-1/Hello.py
@@ -0,0 +1,2 @@
+# This is a comment
+print("Hello, Divyanshu! Welcome to python learning")
---
+++ Day-1/Variable.py
@@ -0,0 +1,8 @@
+name = "Divyanshu" #String
+age = 22           #int
+is_student = True  #bool
+
+print(name, age, is_student)
+print(type(name))
+print(type(age))
+print(type(is_student))
